5faae0ab0b0a27f9723c1796716e971da65f9c56
Stephen Osentoski <stosento@umich.edu>
2021-02-13 10:43:30 -0500

    Updated bio, colors, styling of homepage

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -19,8 +19,9 @@ function HeroSection() {
                         </span>
                     </p>
                     <p id='body-text'>
-                        I'm Stephen, a Michigan-based engineer, video editor, football analyst, and lover of college sports. I currently work at <a href="https://www.avatria.com/" target="blank">Avatria</a>
-                        , helping companies expand their ecommerce capabilities. Take a look around to check out what I've been up to lately.
+                        I'm Stephen, a Michigan-based engineer, consultant, video creator & editor, podcaster, and fanatic of college sports. I currently work at <a href="https://www.avatria.com/" target="blank">Avatria</a>
+                        , helping companies expand their ecommerce capabilities. I contribute to <a href="https://www.maizenbrew.com/" target="blank">MaizenBrew</a>, producing videos on multiple Michigan sports. 
+                        Take a look around to check out what I've been up to lately.
                     </p>
                 </div>
                             
