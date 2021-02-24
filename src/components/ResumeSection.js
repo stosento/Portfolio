df54a79883985f8ed7c128f577a08a11a7aeeb86
Stephen Osentoski <stosento@umich.edu>
2021-02-24 08:45:05 -0500

    Further added styles & components to Resume page

--- src/components/ResumeSection.js
+++ src/components/ResumeSection.js
@@ -9,6 +9,29 @@ function ResumeSection() {
                 <div id='img-sidebar' className='section-header'>
                     <img src={background}/>
                 </div>
+                <div id='header-content-wrapper' class='section-content'>
+                    <h1 id='page-title'>Stephen Osentoski</h1>
+                    <h3 id='page-subtitle'>Engineer in Ecommerce. Believer in robust documentation, challenging projects, and clean integrations</h3>
+                </div>
+            </div>
+            <div className='content-section'>
+                <div id='bg-orange' className='section-header'>
+                    <h2>About Me</h2>
+                </div>
+                <div className='section-content'>
+                    <p> Hello There! I'm Stephen, an application architect newly based in Troy, Michigan. I specialize in SAP Commerce, leading multiple projects in the B2B & B2C space, working with both back-end systems and customer-experience focused projects.</p>
+                    <p> I'm a person</p>
+                </div>
+            </div>
+            <div className='content-section'>
+                <div id='bg-blue' className='section-header'>
+                    <h2>Experience</h2>
+                </div>
+            </div>
+            <div className='content-section'>
+                <div id='bg-red' className='section-header'>
+                    <h2>Languages & Frameworks</h2>
+                </div>
             </div>
         </div>
     )
