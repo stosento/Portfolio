66e56c79dc79f8c2075eeced0571ecd5875d75cc
Stephen Osentoski <stosento@umich.edu>
2021-02-24 23:35:21 -0500

    Updates to Resume

--- src/components/ResumeSection.js
+++ src/components/ResumeSection.js
@@ -19,18 +19,32 @@ function ResumeSection() {
                     <h2>About Me</h2>
                 </div>
                 <div className='section-content'>
-                    <p> Hello There! I'm Stephen, an application architect newly based in Troy, Michigan. I specialize in SAP Commerce, leading multiple projects in the B2B & B2C space, working with both back-end systems and customer-experience focused projects.</p>
-                    <p> I'm a person</p>
+                    <p> Hello There! I'm Stephen, an engineer / architect / consultant newly based in Troy, Michigan. I specialize in SAP Commerce, with experience in both the B2B & B2C space, working on a wide array of projects, from back-end systems to customer-experience focused projects.</p>
+                    <p> I currently work at Avatria as an Application Architect, leading companies to fully realize their eCommerce goals. When I'm not working, you can find me rock climbing, on a run with my dog, or creating videos, likely related to Michigan sports. </p>
+                    <p> Take a look at my recent experience below, and don't hesitate to reach out! </p>
                 </div>
             </div>
             <div className='content-section'>
                 <div id='bg-blue' className='section-header'>
                     <h2>Experience</h2>
                 </div>
+                <div className='section-content'>
+                    <div className='experience-header'>
+                    <h3>AVATRIA</h3>
+                    <h4></h4>
+
+                    </div>
+                    <div className='experience-content'>
+                        
+                    </div>
+                </div>
             </div>
             <div className='content-section'>
                 <div id='bg-red' className='section-header'>
                     <h2>Languages & Frameworks</h2>
+                </div>
+                <div className='section-content'>
+                    
                 </div>
             </div>
         </div>
