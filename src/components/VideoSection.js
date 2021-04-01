b45d4c672b2362b429a56866219befb8b79d7bfa
Stephen Osentoski <stosento@umich.edu>
2021-04-01 08:26:17 -0400

    Adding logos to video page

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import './VideoSection.css';
+import mgofish from '../images/mgofish.png';
+import maizenbrew from '../images/maizenbrew.png';
 
 function VideoSection() {
     return (
         <div className='video-container'>
-            <h2>
-                Coming Soon
-            </h2>
+            <div className='company-container'>
+                <div className='company'>
+                    <a href='https://www.mgofish.com'>
+                        <img src={mgofish} id='mgofish'/>
+                    </a>
+                    <h2>MGOFISH</h2>
+                    <h3>2016 - 2020</h3>
+                </div>
+                <div className='company'>
+                    <a href='https://www.maizenbrew.com'>
+                        <img src={maizenbrew} id='maizenbrew'/>
+                    </a>
+                    <h2>MAIZE N BREW</h2>
+                    <h3>2020 - PRESENT</h3>
+                </div>
+            </div>
+            <div className='sample-container'>
+                <div className='sample'>
+
+                </div>
+                <div className='sample'>
+                    
+                </div>
+                <div className='sample'>
+                    
+                </div>
+            </div>
+            <div className='recent-continer'>
+
+            </div>
         </div>
     )
 }
